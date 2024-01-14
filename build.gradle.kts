a0d4424afe22b226187d92376daefdfbe0c6730c
Ben Basha <ben@bigvu.tv>
2024-01-14 17:11:53 +0200

    revert versions increase

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
+
 buildscript {
 
-    extra["java_version"] = JavaVersion.VERSION_17
-
-    extra["minSdkVersion"] = 21
-    extra["compileSdkVersion"] = 34
-    extra["targetSdkVersion"] = 34
+    extra["minSdkVersion"] = 15
+    extra["compileSdkVersion"] = 31
+    extra["targetSdkVersion"] = 31
 
     repositories {
         google()
@@ -12,9 +11,9 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("com.android.tools.build:gradle:7.0.3")
         classpath("io.deepmedia.tools:publisher:0.6.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
         classpath("com.github.dcendents:android-maven-gradle-plugin:2.0")
 
     }
